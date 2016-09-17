2466d6e7b175f7a0dab7557a7328361f45b3ad2c
Fck-r-sns <fck.r.sns@gmail.com>
2016-09-17 23:16:38 +0300

    Debug triangle intersection check

--- src/scene/Triangle.cpp
+++ src/scene/Triangle.cpp
@@ -9,16 +9,27 @@ Triangle::Triangle(const glm::vec3 &v1, const glm::vec3 &v2, const glm::vec3 &v3
 
 bool Triangle::findIntersection(const Ray &ray, float &minDist, Intersection &result) const
 {
-    const glm::vec3 BminusA = vertices[1] - vertices[0];
-    const glm::vec3 CminusA = vertices[2] - vertices[0];
+    const glm::vec3 &A = vertices[0];
+    const glm::vec3 &B = vertices[1];
+    const glm::vec3 &C = vertices[2];
+    const glm::vec3 BminusA = B - A;
+    const glm::vec3 CminusA = C - A;
     const glm::vec3 n = glm::normalize(glm::cross(BminusA, CminusA));
-    const float t = (glm::dot(vertices[0], n) - glm::dot(ray.origin, n)) / glm::dot(ray.direction, n);
+    const float t = (glm::dot(A, n) - glm::dot(ray.origin, n)) / glm::dot(ray.direction, n);
     if (t > 0 && t < minDist) {
         const glm::vec3 P = ray.origin + t * ray.direction;
-        const glm::vec3 PminusA = P - vertices[0];
+        const glm::vec3 PminusA = P - A;
         // solve equations: P-A=a(B-A)+b(C-A)
-        const float b = (PminusA.y * BminusA.x - PminusA.x * BminusA.y) / (CminusA.y * BminusA.x - CminusA.x * BminusA.y);
-        const float a = (PminusA.x - b * CminusA.x) / BminusA.x;
+        const float PminusA_1 = PminusA.x;
+        const float PminusA_2 = PminusA.y;
+        const float BminusA_1 = BminusA.x;
+        const float BminusA_2 = BminusA.y;
+        const float CminusA_1 = CminusA.x;
+        const float CminusA_2 = CminusA.y;
+        const float b1 = (PminusA_2 * BminusA_1 - PminusA_1 * BminusA_2);
+        const float b2 = (CminusA_2 * BminusA_1 - CminusA_1 * BminusA_2);
+        const float b = b1 / b2;
+        const float a = (PminusA_1 - b * CminusA_1) / BminusA_1;
         if ((0 <= a) && (a <= 1)
                 && (0 <= b) && (b <= 1)
                 && (a + b <= 1)) {
