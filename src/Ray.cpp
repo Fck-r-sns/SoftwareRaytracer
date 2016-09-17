c56c11a53e9391bbf4c6367b12ad40100ffdc0b0
Fck-r-sns <fck.r.sns@gmail.com>
2016-09-17 20:19:10 +0300

    Immutable ray

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -4,13 +4,3 @@ Ray::Ray(const glm::vec3 &origin, const glm::vec3 &direction) :
     origin(origin),
     direction(direction)
 {}
-
-const glm::vec3 &Ray::getOrigin() const
-{
-    return origin;
-}
-
-const glm::vec3 &Ray::getDirection() const
-{
-    return direction;
-}
--- src/Ray.h
+++ src/Ray.h
@@ -7,12 +7,9 @@ class Ray
 {
 public:
     Ray(const glm::vec3 &origin, const glm::vec3 &direction);
-    const glm::vec3 &getOrigin() const;
-    const glm::vec3 &getDirection() const;
 
-private:
-    glm::vec3 origin;
-    glm::vec3 direction;
+    const glm::vec3 origin;
+    const glm::vec3 direction;
 };
 
 #endif // RAY_H
