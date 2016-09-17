0a612f97e06759a98eb6cf60431a0c49001286bd
Fck-r-sns <fck.r.sns@gmail.com>
2016-09-17 22:12:08 +0300

    Image writing fixed

--- src/image/ImageWriter.cpp
+++ src/image/ImageWriter.cpp
@@ -14,7 +14,6 @@ ImageWriter::~ImageWriter()
 bool ImageWriter::saveImage(const std::string &fileName, Image &image, int width, int height)
 {
     BYTE *bytes = reinterpret_cast<BYTE*>(image.getData());
-    FIBITMAP *img = FreeImage_ConvertFromRawBits(bytes, width, height, width * 3, 24, 0, 0, 0, false);
-    const bool isOk = FreeImage_Save(FIF_PNG, img, fileName.c_str(), 0);
-    return isOk;
+    FIBITMAP *img = FreeImage_ConvertFromRawBits(bytes, width, height, width * 3, 24, 0, 0, 0, true);
+    return FreeImage_Save(FIF_PNG, img, fileName.c_str(), 0);
 }
