b4aa700ee9d3c46d0ef6b37f1736df240d47dd95
Fck-r-sns <fck.r.sns@gmail.com>
2016-09-18 00:29:03 +0300

    Triangle intersection check fixed

--- src/scene/Triangle.cpp
+++ src/scene/Triangle.cpp
@@ -1,8 +1,8 @@
 #include "Triangle.h"
 #include "libs/glm/gtc/matrix_transform.hpp"
 
-Triangle::Triangle(const glm::vec3 &v1, const glm::vec3 &v2, const glm::vec3 &v3, const Material &material) :
-    Primitive(material),
+Triangle::Triangle(const glm::vec3 &v1, const glm::vec3 &v2, const glm::vec3 &v3, int materialIndex) :
+    Primitive(materialIndex),
     vertices({v1, v2, v3})
 {
 }
@@ -20,13 +20,15 @@ bool Triangle::findIntersection(const Ray &ray, float &minDist, Intersection &re
         const glm::vec3 P = ray.origin + t * ray.direction;
         const glm::vec3 PminusA = P - A;
         // solve equations: P-A=a(B-A)+b(C-A)
+        // TODO: refactor this shit
         float PminusA_1;
         float PminusA_2;
         float BminusA_1;
         float BminusA_2;
         float CminusA_1;
         float CminusA_2;
-        if (BminusA.x != 0.0) {
+        bool ready = false;
+        if (!ready && BminusA.x != 0.0) {
             PminusA_1 = PminusA.x;
             BminusA_1 = BminusA.x;
             CminusA_1 = CminusA.x;
@@ -34,12 +36,15 @@ bool Triangle::findIntersection(const Ray &ray, float &minDist, Intersection &re
                 PminusA_2 = PminusA.y;
                 BminusA_2 = BminusA.y;
                 CminusA_2 = CminusA.y;
-            } else {
+                ready = true;
+            } else if (CminusA.z != 0.0) {
                 PminusA_2 = PminusA.z;
                 BminusA_2 = BminusA.z;
                 CminusA_2 = CminusA.z;
+                ready = true;
             }
-        } else if (BminusA.y != 0) {
+        }
+        if (!ready && BminusA.y != 0) {
             PminusA_1 = PminusA.y;
             BminusA_1 = BminusA.y;
             CminusA_1 = CminusA.y;
@@ -47,12 +52,15 @@ bool Triangle::findIntersection(const Ray &ray, float &minDist, Intersection &re
                 PminusA_2 = PminusA.x;
                 BminusA_2 = BminusA.x;
                 CminusA_2 = CminusA.x;
-            } else {
+                ready = true;
+            } else if (CminusA.z != 0.0) {
                 PminusA_2 = PminusA.z;
                 BminusA_2 = BminusA.z;
                 CminusA_2 = CminusA.z;
+                ready = true;
             }
-        } else if (BminusA.z != 0) {
+        }
+        if (!ready && BminusA.z != 0) {
             PminusA_1 = PminusA.z;
             BminusA_1 = BminusA.z;
             CminusA_1 = CminusA.z;
@@ -60,12 +68,15 @@ bool Triangle::findIntersection(const Ray &ray, float &minDist, Intersection &re
                 PminusA_2 = PminusA.x;
                 BminusA_2 = BminusA.x;
                 CminusA_2 = CminusA.x;
-            } else {
+                ready = true;
+            } else if (CminusA.y != 0.0) {
                 PminusA_2 = PminusA.y;
                 BminusA_2 = BminusA.y;
                 CminusA_2 = CminusA.y;
+                ready = true;
             }
-        } else {
+        }
+        if (!ready) {
             return false;
         }
         const float b1 = (PminusA_2 * BminusA_1 - PminusA_1 * BminusA_2);
@@ -77,6 +88,7 @@ bool Triangle::findIntersection(const Ray &ray, float &minDist, Intersection &re
                 && (a + b <= 1)) {
             minDist = t;
             result.t = t;
+            result.point = P;
             result.primitive = this;
             return true;
         }
--- src/scene/Triangle.h
+++ src/scene/Triangle.h
@@ -6,7 +6,7 @@
 
 class Triangle : public Primitive {
 public:
-    Triangle(const glm::vec3 &v1, const glm::vec3 &v2, const glm::vec3 &v3, const Material &material);
+    Triangle(const glm::vec3 &v1, const glm::vec3 &v2, const glm::vec3 &v3, int materialIndex);
     bool findIntersection(const Ray &ray, float &minDist, Intersection &result) const override;
 
 private:
