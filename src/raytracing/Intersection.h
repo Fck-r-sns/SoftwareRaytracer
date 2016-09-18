999c67a36b6058cf28260577e5a52240b9673b6f
Fck-r-sns <fck.r.sns@gmail.com>
2016-09-18 12:33:42 +0300

    Normal added to intersection

--- src/raytracing/Intersection.h
+++ src/raytracing/Intersection.h
@@ -15,7 +15,9 @@ public:
     bool empty = true;
     float t = std::numeric_limits<float>::max();
     glm::vec3 point;
+    glm::vec3 normal;
     const Primitive *primitive = nullptr;
+    static const Intersection EMPTY;
 };
 
 #endif // INTERSECTION_H
