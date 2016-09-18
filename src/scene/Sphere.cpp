67ce18770f633410a58367a799a428010ab26f57
Fck-r-sns <fck.r.sns@gmail.com>
2016-09-18 12:38:47 +0300

    Sphere normal calculation

--- src/scene/Sphere.cpp
+++ src/scene/Sphere.cpp
@@ -10,15 +10,13 @@ Sphere::Sphere(const glm::vec3 &position, float radius, int materialIndex, const
 
 Intersection Sphere::findIntersection(const Ray &ray, float &minDist) const
 {
-    Intersection result;
     const float a = glm::dot(ray.direction, ray.direction);
     const glm::vec3 PminusC = ray.origin - position;
     const float b = 2 * glm::dot(ray.direction, PminusC);
     const float c = glm::dot(PminusC, PminusC) - radius * radius;
     const float D = b * b - 4 * a * c;
     if (D < 0) {
-        result.empty = true;
-        return result;
+        return Intersection::EMPTY;
     }
     const float sqrtD = std::sqrt(D);
     const float t1 = (-b - sqrtD) / (2 * a);
@@ -26,12 +24,13 @@ Intersection Sphere::findIntersection(const Ray &ray, float &minDist) const
     const float t = (t1 > 0) ? t1 : t2;
     if (t > 0 && t < minDist) {
         minDist = t;
+        Intersection result;
         result.t = t;
         result.point = ray.origin + t * ray.direction;
+        result.normal = glm::normalize(result.point - position);
         result.primitive = this;
         result.empty = false;
         return result;
     }
-    result.empty = true;
-    return result;
+    return Intersection::EMPTY;
 }
