ec9b7f63bab18893c7771336883abee4c96643a8
Fck-r-sns <fck.r.sns@gmail.com>
2016-09-19 00:11:23 +0300

    execution time output

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ctime>
+#include <chrono>
 #include "scene/SceneConfiguration.h"
 #include "scene/Parser.h"
 #include "image/Image.h"
@@ -10,11 +12,20 @@ int main(int argc, char *argv[])
     if (argc < 2) {
         return 1;
     }
+
+    time_t started = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::cout << "started: " << ctime(&started);
+
     const std::string inputFileName = argv[1];
     SceneConfiguration cfg = Parser::parse(inputFileName);
     Image image(cfg.width, cfg.height);
     RayTracer rayTracer(cfg);
     rayTracer.run(image);
     ImageWriter().saveImage(cfg.outputFileName, image, cfg.width, cfg.height);
+
+    time_t finished = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::cout << "started: " << ctime(&started);
+    std::cout << "finished: " << ctime(&finished);
+
     return 0;
 }
